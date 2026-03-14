8bec8323697a51c0863b74f8cc35ae473cdcb8d5
Stefania Makrygiannaki <stefaniamakk@gmail.com>
2026-03-14 14:56:52 +0200

    adds dart logo

---
+++ lib/consts/assets/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static final String _assetsPath = '../assets/';
+
+  static String dartLogo = '${_assetsPath}dart-programming-language-icon.svg';
+  static String dartLogoUrl = 'https://dart.dev/assets/img/logo/dart-192.svg';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dart_dad/consts/assets/app_assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'DartDad',
+      home: const MyHomePage(title: 'DartDad'),
     );
   }
 }
@@ -34,16 +33,20 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Color(0xFF1e2833),
+
+        title: Row(
+          children: [
+            SvgPicture.network(AppAssets.dartLogoUrl, width: 24),
+            Text(widget.title),
+          ],
+        ),
+        titleTextStyle: TextStyle(color: Colors.white),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text('a', style: Theme.of(context).textTheme.headlineMedium),
-          ],
+          children: <Widget>[],
         ),
       ),
     );
